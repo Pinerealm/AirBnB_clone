050e8ef07ec3d5b2934b9e346830bab2977017bb
Pinerealm <osalami09@gmail.com>
2022-10-29 20:28:41 +0300

    Make two spaces before inline comment

--- models/place.py
+++ models/place.py
@@ -15,7 +15,7 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = [] # list of strings
+    amenity_ids = []  # list of strings
 
     def __init__(self, *args, **kwargs):
         """Runs on initialization"""
